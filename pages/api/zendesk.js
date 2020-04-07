3130c583b07803475523eec0261c778cae91f667
Dieter Evangelista <hansevangelista@gmail.com>
2020-04-07 08:03:12 -0500

    add new costs

--- pages/api/zendesk.js
+++ pages/api/zendesk.js
@@ -4,20 +4,33 @@ const axios = require('axios')
 const superagent = require("superagent");
 const handler = (req, res) => {
 
-  const URL = 'https://insights-api.newrelic.com/v1/accounts/2482859/query?nrql=SELECT%20*%20FROM%20zendeskStatus%20SINCE%203%20week%20ago'
+  let URL = 'https://insights-api.newrelic.com/v1/accounts/2482859/query?nrql=SELECT%20*%20FROM%20zendeskCost%20SINCE%203%20week%20ago'
   const headers = { "X-Query-Key": 'NRIQ-V-S-D7Tgcd4R7BHtumVd8QYaLVxzTDXG' }
 
   axios.get(URL, { headers })
   .then(response => {
-    let status = response.data.results[0].events
+    let data = response.data.results[0].events[0]
 
-    status = status.filter(state => state.type )
-    console.log('status: ', status)
+    let ret = {
+      totalUsers: data.totalUsers,
+      activeUsers: data.activeUsers,
+      instanceType: data.type
+    }
 
-    let ret = { status }
-    // console.log('ret: ', ret)
 
-    return res.json(ret)
-   })
+    URL = 'https://insights-api.newrelic.com/v1/accounts/2482859/query?nrql=SELECT%20*%20FROM%20zendeskStatus%20SINCE%203%20week%20ago'
+
+    axios.get(URL, { headers })
+    .then(response => {
+      let status = response.data.results[0].events
+
+      status = status.filter(state => state.type )
+
+      ret.status = status
+
+      return res.json(ret)
+     })
+
+  })
 }
 export default cors(handler)
